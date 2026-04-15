03f433a01236b201a9d69c3e2a30a34a8bfbcdeb
LegoWolf <glynwolf@gmail.com>
2026-04-15 15:29:31 -0600

    Tidied error handling and reporting.

--- image_monitor.py
+++ image_monitor.py
@@ -94,46 +94,6 @@ class Classifier:
         logging.debug(f"Found {len(results)} tags above INTERNAL threshold {INTERNAL_THRESHOLD} and with a score above {score_cutoff}.")
         return [result[0] for result in results], end_preprocess - start_preprocess, end_inference - start_inference
 
-def run_command(args, input=None) -> bool:
-    process = subprocess.run(
-        args,
-        input=input,
-        encoding='utf-8',
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    return process.returncode, process.stdout.strip(), process.stderr.strip()
-
-def check_has_xmp_tag(image_path, tag):
-    return_code, stdout, stderr = run_command(['exiv2', '-px', 'pr', image_path])
-    if return_code == 0:
-        for error in stderr.splitlines():
-            logging.warning(f'{error} (while checking tags on: {image_path})')
-        has_tag = tag in stdout
-        logging.info(f'{"Has" if has_tag else "Missing"} JTP-3 tag: {image_path}')
-        return has_tag
-    else:
-        logging.error(f'Failed to find tag "{tag}" in {image_path} ({stderr})')
-        return True
-
-def write_xmp_tags(image_path, tags):
-    try:
-        if len(tags) > 0:
-            # Preserve the current modified time.
-            modified_time = os.path.getmtime(image_path)
-            keywords = [tag.replace('_', ' ') for tag in tags]
-            keywords_buffer = '\n'.join([f'set Xmp.dc.subject {kw}' for kw in keywords])
-            return_code, stdout, stderr = run_command(['exiv2', '-m-', image_path], input=keywords_buffer)
-            if return_code == 0:
-                for error in stderr.splitlines():
-                    logging.warning(f'{error} (while setting tags on: {image_path})')
-                logging.debug(f"Wrote XMP keywords to: {image_path}")
-            else:
-                logging.error(f'Failed to write XMP tags to {image_path} ({stderr})')
-            access_time = os.path.getatime(image_path)
-            os.utime(image_path, times=(access_time, modified_time))
-    except Exception as e:
-        logging.error(f"Failed to write to {image_path}: {e}")
-
 class DelayQueue:
     def __init__(self, queue):
         self.queue = queue
@@ -166,6 +126,37 @@ class DelayQueue:
         (item, timestamp) = self.delay_queue.popitem()
         return item, timestamp, self.delay_info.pop(item)
 
+def run_command(args, input=None) -> bool:
+    process = subprocess.run(
+        args,
+        input=input,
+        encoding='utf-8',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True)
+    return process.returncode, process.stdout.strip(), process.stderr.strip()
+
+def check_has_xmp_tag(image_path, tag):
+    return_code, stdout, stderr = run_command(['exiv2', '-px', 'pr', image_path])
+    for error in stderr.splitlines():
+        logging.warning(f'{error} (while checking tags on: {image_path})')
+    has_tag = tag in stdout
+    logging.debug(f'{"Has" if has_tag else "Missing"} JTP-3 tag: {image_path}')
+    return has_tag
+
+def write_xmp_tags(image_path, tags):
+    if len(tags) > 0:
+        # Preserve the current modified time.
+        modified_time = os.path.getmtime(image_path)
+        keywords = [tag.replace('_', ' ') for tag in tags]
+        keywords_buffer = '\n'.join([f'set Xmp.dc.subject {kw}' for kw in keywords])
+        return_code, stdout, stderr = run_command(['exiv2', '-m-', image_path], input=keywords_buffer)
+        for error in stderr.splitlines():
+            logging.warning(f'{error} (while setting tags on: {image_path})')
+        logging.debug(f"Wrote XMP keywords to: {image_path}")
+        access_time = os.path.getatime(image_path)
+        os.utime(image_path, times=(access_time, modified_time))
+
 def image_processor(image_queue):
     delay_queue = DelayQueue(image_queue)
     classifier = Classifier(model_path=MODEL_PATH)
@@ -188,9 +179,11 @@ def image_processor(image_queue):
                     tags.append(config["classified_tag"])
                     write_xmp_tags(image_path, tags)
                     logging.info(f'{event:8} {time_delay:2.2f}s {time_preprocess:2.2f}s {time_inference:2.2f}s {len(tags):3} {image_path}')
+            except subprocess.CalledProcessError as e:
+                logging.error(f"Called process '{' '.join(e.cmd)}' failed: {e.stderr.strip()} ({e.returncode})")
             except Exception as e:
-                logging.error(f"ERROR during JTP-3 analysis: {e}")
-                logging.error(f"Stack trace:\n{traceback.format_exc()}")
+                logging.error(f"Image processing failed: {e} ({image_path})")
+                logging.debug(f"Stack trace:\n{traceback.format_exc().strip()}")
 
 def main():
     import __main__
